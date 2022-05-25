e1986c3abb3ab2f2ef91a992f4bdf4eb0e767d7e
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-05-25 17:26:01 +0200

    creating higher-order component for the header

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import { Router, Switch, Route } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import App from './components/App';
 import Jokes from './components/Jokes'
+import Header from './components/Header';
 import './index.css'
 
 ReactDOM.render(
     <Router history={createBrowserHistory()}>
         <Switch>
-            <Route exact path='/' component={App} />
-            <Route path='/jokes' component={Jokes} />
+            <Route exact path='/' render={() => <Header><App /></Header>} />
+            <Route path='/jokes' render={() => <Header><Jokes /></Header>} />
         </Switch>
     </Router>, 
     document.getElementById('root')
