65cd30da42880cf41626f498596964bba2c433bb
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-05-25 15:58:02 +0200

    creating 2 more components with fetch

---
+++ src/components/Jokes.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+const Joke = ({ joke: { setup, delivery } }) => (
+    <p style={{ margin: 20 }}>{setup} <em>{delivery}</em></p>
+)
+
+class Jokes extends Component {
+    state = { joke: {}, jokes: [] };
+
+    componentDidMount() {
+        fetch('https://v2.jokeapi.dev/joke/Programming?type=twopart')
+            .then(response => response.json())
+            .then(json => this.setState({ joke: json }))
+            .catch(error => alert(error.message));
+    }
+    
+    fetchJokes = () => {
+        fetch('https://v2.jokeapi.dev/joke/Programming?type=twopart&amount=10')
+        .then(response => response.json())
+        .then(({ jokes }) => this.setState({ jokes }))
+        .catch(error => alert(error.message));
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Joke of the day!</h2>
+                <Joke joke={this.state.joke} />
+                <hr />
+                <h3>Want ten new jokes?</h3>
+                <button onClick={this.fetchJokes}>Click me!</button>
+                {this.state.jokes.map(joke => (<Joke key={joke.id} joke={joke}/>))}
+            </div>
+        )
+    }
+}
+
+export default Jokes;
---
+++ src/components/Superheros.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class Superheros extends Component {
+    state = { superhero: {} };
+
+    componentDidMount() {
+        fetch('https://www.superheroapi.com/api.php/5752439914784969/263/work')
+            .then(response => response.json())
+            .then(json => this.setState({ superhero: json }))
+            .catch(error => alert(error.message));
+    }
+
+    render() {
+        const { name, occupation, base} = this.state.superhero;
+
+        return (
+            <div>
+                <h2>Superhero of the day is {name}!</h2>
+                <p>{occupation} <em>{base}</em></p>
+            </div>
+        )
+    }
+}
+
+export default Superheros;
