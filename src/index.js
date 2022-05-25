efde19ef6d2906a95c40806720cdffd68c944fd5
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-05-25 15:58:41 +0200

    adding react router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App'
+import { Router, Switch, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+import App from './components/App';
+import Jokes from './components/Jokes'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router history={createBrowserHistory()}>
+        <Switch>
+            <Route exact path='/' component={App} />
+            <Route path='/jokes' component={Jokes} />
+        </Switch>
+    </Router>, 
+    document.getElementById('root')
+);
